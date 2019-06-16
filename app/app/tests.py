c97329a2348b8ecd2677dfc0b33c112c4b4a35e6
Robert Selaru <robert@{pcextreme, sol}.nl>
2019-06-16 16:41:00 +0200

    fast commit|automated

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
+
 from app.calc import add
+
 
 class CalcTests(TestCase):
 
-    '''The test functions must begin with test.'''
     def test_add_numbers(self):
-        '''Test that two numbers are added together'''
-        self.assertEqual(add(3,8), 11)
+        """Test that values are added together"""
+        self.assertEqual(add(3, 8), 11)
